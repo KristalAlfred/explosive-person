58537743b814f62d5e575b53154481b741a75694
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-05-30 20:08:05 +0200

    remove debug statement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("PlayerController script started");
     }
 
     // Update is called once per frame
